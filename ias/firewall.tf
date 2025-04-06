29a7a7c74c858ff622d36ed4a8c23678cfe6538a
Wissem ABDELJAOUAD <wissem.abdeljaouad@adventium.fr>
2025-04-06 09:41:50 +0000

    Adding terraform configuration

---
+++ ias/firewall.tf
@@ -0,0 +1,12 @@
+resource "google_compute_firewall" "allow_airflow" {
+  name    = "allow-airflow"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["airflow"]
+}
